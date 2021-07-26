9a4f362d07a9dea5ad37289345dde37153124ddd
SB <sb@SARARATs-MacBook-Pro.local>
2021-07-26 15:01:03 -0400

    created Resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../stylesheet/Resume.css';
 
 function Resume() {
     return (
+        <>
         <div>
-            <h1>Resume</h1>
+            <h1 className="page-title">Resume</h1>
+            <h3>Sararat Burke</h3>
+                <h4>Web Designer | Developer</h4>
+            <div className="wrap-table">
+                
+                <h3>Education</h3>
+                <h3>Experience</h3>
+            </div>
+            <div className="wrap-table">
+                <h3>Projects</h3>
+                <h3>Skills</h3>
+            </div>
         </div>
+        </>
     )
 }
 
