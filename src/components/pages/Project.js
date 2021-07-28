54311e4d0ca97add499eec59fdfff7b9683f01d3
SB <sb@SARARATs-MacBook-Pro.local>
2021-07-28 19:44:29 -0400

    added project content

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import '../stylesheet/Project.css'
+import JsonFinal from '../img/json-final.jpg'
+import Todo from '../img/todo-react.jpg'
+import Art from '../img/art-port.jpg'
 
 function Project() {
     return (
-        <div>
-            <h1>Project</h1>
+        <>
+        <div className="project-page">
+        <h1 className="page-title project-header-title">Project</h1>
+            <div className="project-content">
+                <a href="#">
+                    <img className="project-img" src={JsonFinal}></img>
+                </a>
+                <a href="https://todoapp-w4.web.app/" target='_blank'>
+                    <img className="project-img" src={Todo}></img>
+                </a>
+                <a href="http://bdcteach.com/cmt241/burke/xml_final/index.html" target='_blank'>
+                    <img className="project-img" src={Art}></img>
+                </a>
+            </div>
+            
         </div>
+        </>
     )
 }
 
