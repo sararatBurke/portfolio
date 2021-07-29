a58c21ef3e755221a8e523a69aa051b4224b126b
SB <sb@SARARATs-MacBook-Pro.local>
2021-07-29 17:08:53 -0400

    styled contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import '../stylesheet/Contact.css'
+import * as ioIcon from "react-icons/io5";
 
 function Contact() {
     return (
@@ -8,23 +10,22 @@ function Contact() {
            
             <h1 className="page-title contact-header-title">Job Interested</h1>
                 <ul>
-                    <li> <input type="checkbox" checked="checked" ></input> Web Development</li>
-                    <li> <input type="checkbox" checked="checked" ></input> Font-End Development</li>
-                    <li> <input type="checkbox" checked="checked" ></input> Mobile Developmemt</li>
-                    <li> <input type="checkbox" checked="checked" ></input> Web Design</li>
-                    <li> <input type="checkbox" checked="checked" ></input> Graphic Design</li>
+                    <li className="interested-list"> <ioIcon.IoCheckmarkSharp/> Web Development</li>
+                    <li className="interested-list"> <ioIcon.IoCheckmarkSharp/> Font-End Development</li>
+                    <li className="interested-list"> <ioIcon.IoCheckmarkSharp/> Mobile Developmemt</li>
+                    <li className="interested-list"> <ioIcon.IoCheckmarkSharp/> Web Design</li>
+                    <li className="interested-list"> <ioIcon.IoCheckmarkSharp/> Graphic Design</li>
 
                 </ul>
             </div>
            
             <div className="contact-content">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr9J8RyyCES4TyaNx6AkmdvKVxw8CcxXmzgqswcT1B3yuMBt4eo-Q9le-A-vWlvACvAQ&usqp=CAU"></img>                
-                <p>Sararat Burke</p>
-                <p>860.3252946</p>
-                <p>baas.burke@gmail.com</p>
-                <p>435 E Broadway, Haverhille, MA 01830</p>
-                <p><a href="https://www.linkedin.com/in/sararat-burke-869497b9/">LinkIned</a></p>
-                <p><a href="https://github.com/sararatBurke">GitHub</a></p>
+                    <img className="stamp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr9J8RyyCES4TyaNx6AkmdvKVxw8CcxXmzgqswcT1B3yuMBt4eo-Q9le-A-vWlvACvAQ&usqp=CAU"></img>                
+                <p className="name">Sararat Burke</p>
+                <p className="contact-info">435 E Broadway, <br/>Haverhille, MA 01830</p>
+                <p className="contact-info">baas.burke@gmail.com</p>
+                <p className="contact-info"><a className="contact-link" target="_blank" href="https://www.linkedin.com/in/sararat-burke-869497b9/">LinkIned</a></p>
+                <p className="contact-info"><a className="contact-link" target="_blank" href="https://github.com/sararatBurke">GitHub</a></p>
 
             </div>
         </div>
